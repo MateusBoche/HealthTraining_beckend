0c91593993039975d99fe76494df562dfb2f7737
Mateus Boche <mateussenaisrs@gmail.com>
2024-09-11 08:19:25 -0300

    feat: criacao do projeto, adicao de dependencias, adicao dos pacotes esqueletos do projeto

---
+++ src/main/java/br/fai/backend/heathtraining/beckend/healthtraining/HealthtrainingApplication.java
@@ -0,0 +1,13 @@
+package br.fai.backend.heathtraining.beckend.healthtraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthtrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthtrainingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/fai/backend/heathtraining/beckend/healthtraining/HealthtrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package br.fai.backend.heathtraining.beckend.healthtraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthtrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
