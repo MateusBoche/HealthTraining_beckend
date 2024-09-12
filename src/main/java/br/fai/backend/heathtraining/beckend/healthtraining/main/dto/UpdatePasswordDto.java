1b16caa7d81d8f63489055ac1a0020d1b97eb3cf
dilton <diltonthales.dt@gmail.com>
2024-09-12 11:07:21 -0300

    refactor: ajuste UpdatePasswordDto

--- src/main/java/br/fai/backend/heathtraining/beckend/healthtraining/main/dto/UpdatePasswordDto.java
+++ src/main/java/br/fai/backend/heathtraining/beckend/healthtraining/main/dto/UpdatePasswordDto.java
@@ -1,4 +1,10 @@
 package br.fai.backend.heathtraining.beckend.healthtraining.main.dto;
 
+import lombok.Data;
+
+@Data
 public class UpdatePasswordDto {
+    private int id;
+    private String oldPassword;
+    private String newPassword;
 }
