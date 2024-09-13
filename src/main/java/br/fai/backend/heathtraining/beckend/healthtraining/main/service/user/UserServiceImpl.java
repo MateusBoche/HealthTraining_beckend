c2045ccab536514ef563d008acf5143801dd6b50
Mateus Boche <mateussenaisrs@gmail.com>
2024-09-13 14:45:57 -0300

    feat: criando esqueleto de UserServiceImpl

--- src/main/java/br/fai/backend/heathtraining/beckend/healthtraining/main/service/user/UserServiceImpl.java
+++ src/main/java/br/fai/backend/heathtraining/beckend/healthtraining/main/service/user/UserServiceImpl.java
@@ -1,4 +1,38 @@
 package br.fai.backend.heathtraining.beckend.healthtraining.main.service.user;
 
-public class UserServiceImpl implements {
+import br.fai.backend.heathtraining.beckend.healthtraining.main.domain.UserModel;
+import br.fai.backend.heathtraining.beckend.healthtraining.main.port.service.user.UserService;
+
+import java.util.List;
+
+public class UserServiceImpl implements UserService {
+    @Override
+    public int add(UserModel entity) {
+        return 0;
+    }
+
+    @Override
+    public void remove(int id) {
+
+    }
+
+    @Override
+    public UserModel readById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<UserModel> readAll() {
+        return null;
+    }
+
+    @Override
+    public void update(int id, UserModel entity) {
+
+    }
+
+    @Override
+    public boolean updatePassword(int id, String newPassword) {
+        return false;
+    }
 }
