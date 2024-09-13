8095001ca17a1f3a02d8064e2419c570ff759d53
Mateus Boche <mateussenaisrs@gmail.com>
2024-09-13 15:08:27 -0300

    feat: começando a desenvolver o UserRestController

--- src/main/java/br/fai/backend/heathtraining/beckend/healthtraining/main/controller/UserRestController.java
+++ src/main/java/br/fai/backend/heathtraining/beckend/healthtraining/main/controller/UserRestController.java
@@ -1,10 +1,15 @@
 package br.fai.backend.heathtraining.beckend.healthtraining.main.controller;
 
+import br.fai.backend.heathtraining.beckend.healthtraining.main.port.service.user.UserService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/user")
 public class UserRestController {
+    private final UserService userService;
 
+    public UserRestController(UserService userService) {
+        this.userService = userService;
+    }
 }
