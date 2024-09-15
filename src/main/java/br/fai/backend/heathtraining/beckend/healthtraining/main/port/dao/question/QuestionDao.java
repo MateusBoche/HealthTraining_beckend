d1ba1bc2d8199e8277606b6679604b357c0463fe
Mateus Boche <mateussenaisrs@gmail.com>
2024-09-15 11:37:38 -0300

    feat: implementando QuestionDao

---
+++ src/main/java/br/fai/backend/heathtraining/beckend/healthtraining/main/port/dao/question/QuestionDao.java
@@ -0,0 +1,6 @@
+package br.fai.backend.heathtraining.beckend.healthtraining.main.port.dao.question;
+
+import br.fai.backend.heathtraining.beckend.healthtraining.main.port.dao.crud.CrudDao;
+
+public interface QuestionDao extends CrudDao {
+}
