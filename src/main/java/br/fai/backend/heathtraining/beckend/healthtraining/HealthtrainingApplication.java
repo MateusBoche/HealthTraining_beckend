6c7d0895a2040201effa0f80398d8433107afb77
Mateus Boche <mateussenaisrs@gmail.com>
2024-09-15 11:38:07 -0300

    refactor: mudanças para ver se corrige os erros

--- src/main/java/br/fai/backend/heathtraining/beckend/healthtraining/HealthtrainingApplication.java
+++ src/main/java/br/fai/backend/heathtraining/beckend/healthtraining/HealthtrainingApplication.java
@@ -1,13 +1,24 @@
 package br.fai.backend.heathtraining.beckend.healthtraining;
 
+import br.fai.backend.heathtraining.beckend.healthtraining.main.port.service.util.ResourceFileService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
 public class HealthtrainingApplication {
 
 
-	public static void main(String[] args) {
+	@Autowired
+	private ResourceFileService resourceFileService;
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+
+
+  public static void main(String[] args) {
 		SpringApplication.run(HealthtrainingApplication.class, args);
 	}
 
