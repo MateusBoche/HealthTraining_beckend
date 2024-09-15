7a35df286abd5516e7e96bc27dc543311fe0e410
Mateus Boche <mateussenaisrs@gmail.com>
2024-09-15 11:37:23 -0300

    feat: implementando QuestionServiceImpl

---
+++ src/main/java/br/fai/backend/heathtraining/beckend/healthtraining/main/service/question/QuestionServiceImpl.java
@@ -0,0 +1,71 @@
+package br.fai.backend.heathtraining.beckend.healthtraining.main.service.question;
+
+import br.fai.backend.heathtraining.beckend.healthtraining.main.domain.GameModel;
+import br.fai.backend.heathtraining.beckend.healthtraining.main.domain.QuestionModel;
+import br.fai.backend.heathtraining.beckend.healthtraining.main.domain.UserModel;
+import br.fai.backend.heathtraining.beckend.healthtraining.main.port.dao.question.QuestionDao;
+import br.fai.backend.heathtraining.beckend.healthtraining.main.port.service.game.GameService;
+import br.fai.backend.heathtraining.beckend.healthtraining.main.port.service.question.QuestionService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class QuestionServiceImpl implements QuestionService {
+
+  private final QuestionDao questionDao;
+
+  public QuestionServiceImpl(QuestionDao questionDao) {
+    this.questionDao = questionDao;
+  }
+
+
+  @Override
+  public void delete(int id) {
+    if(id<0){
+      return;
+    }
+    questionDao.remove(id);
+
+  }
+
+  @Override
+  public QuestionModel findById(int id) {
+    if(id<0){
+      return null;
+    }
+    QuestionModel question = (QuestionModel) questionDao.readById(id);
+    return question;
+
+  }
+
+  @Override
+  public List<QuestionModel> findAll() {
+    List<QuestionModel> questionModels = questionDao.readAll();
+    return questionModels;
+  }
+
+
+  @Override
+  public int create(QuestionModel entity) {
+    if (entity == null) {
+      return 0;
+    }
+    if (entity.getQuestion().isEmpty() || entity.getCategory().isEmpty()) {
+      return 0;
+
+    }
+    int id = questionDao.add(entity);
+    return id;
+  }
+
+  @Override
+  public void update(int id, QuestionModel entity) {
+    QuestionModel question = findById(id);
+    if(question == null){
+      return;
+    }
+    questionDao.updateInformation(id,entity);
+
+  }
+}
