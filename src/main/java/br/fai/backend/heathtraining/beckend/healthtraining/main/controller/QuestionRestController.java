b62a30dd467a32a2726a5379d89bc07a3251e1f3
Mateus Boche <mateussenaisrs@gmail.com>
2024-09-15 12:00:07 -0300

    feat: implementado o createEntity nas QuestionRestController

--- src/main/java/br/fai/backend/heathtraining/beckend/healthtraining/main/controller/QuestionRestController.java
+++ src/main/java/br/fai/backend/heathtraining/beckend/healthtraining/main/controller/QuestionRestController.java
@@ -5,11 +5,10 @@ import br.fai.backend.heathtraining.beckend.healthtraining.main.domain.GameModel
 import br.fai.backend.heathtraining.beckend.healthtraining.main.domain.QuestionModel;
 import br.fai.backend.heathtraining.beckend.healthtraining.main.port.service.question.QuestionService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -40,6 +39,18 @@ public class QuestionRestController {
     QuestionModel question = questionService.findByCategory(category);
     return ResponseEntity.ok().body(question);
   }
+
+  @PostMapping()
+  public ResponseEntity<QuestionModel> createEntity(@RequestBody final QuestionModel data){
+    int id = questionService.create(data);
+    final URI uri = ServletUriComponentsBuilder
+      .fromCurrentRequest()
+      .path(("/{id}"))
+      .buildAndExpand(id)
+      .toUri();
+    return ResponseEntity.created(uri).build();
+  }
+
 
 
 
