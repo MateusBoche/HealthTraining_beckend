25487505fadf20f36f2dcf7dbd485f3795a5d43e
dilton <diltonthales.dt@gmail.com>
2024-09-15 09:35:22 -0300

    feat: criacao do arquivo de configuracoes

--- src/main/java/br/fai/backend/heathtraining/beckend/healthtraining/main/configuration/AppConfiguration.java
+++ src/main/java/br/fai/backend/heathtraining/beckend/healthtraining/main/configuration/AppConfiguration.java
@@ -1,6 +1,27 @@
 package br.fai.backend.heathtraining.beckend.healthtraining.main.configuration;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@Configuration
 public class AppConfiguration {
+
+    public  AppConfiguration(){
+        System.out.println("aaaaaaaaaaaaa");
+    }
+
+//    @Bean
+//    @Profile("fake")
+//    public UserDao getUserFakeDao(){
+//        return new UserFakeDaoImpl();
+//    }
+//    @Bean
+//    @Profile("dev")
+//    public UserDao getH2Dao(final JdbcTemplate jdbcTemplate){
+//        return new UserH2DaoImpl(jdbcTemplate);
+//    }
 
 
 }
