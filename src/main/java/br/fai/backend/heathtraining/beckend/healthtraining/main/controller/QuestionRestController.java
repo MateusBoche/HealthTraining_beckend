a47f3c0ea30a192f870a28b45b973c124c750fea
Mateus Boche <mateussenaisrs@gmail.com>
2024-09-15 11:43:34 -0300

    feat: implementacao do QuesitonRestController implementado o getEntities e o getEntityByID

--- src/main/java/br/fai/backend/heathtraining/beckend/healthtraining/main/controller/QuestionRestController.java
+++ src/main/java/br/fai/backend/heathtraining/beckend/healthtraining/main/controller/QuestionRestController.java
@@ -1,4 +1,39 @@
 package br.fai.backend.heathtraining.beckend.healthtraining.main.controller;
 
+
+import br.fai.backend.heathtraining.beckend.healthtraining.main.domain.GameModel;
+import br.fai.backend.heathtraining.beckend.healthtraining.main.domain.QuestionModel;
+import br.fai.backend.heathtraining.beckend.healthtraining.main.port.service.question.QuestionService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/question")
 public class QuestionRestController {
+  private final QuestionService questionService;
+
+  public QuestionRestController(QuestionService questionService) {
+    this.questionService = questionService;
+  }
+
+
+  @GetMapping()
+  public ResponseEntity<List<QuestionModel>> getEntities(){
+    List<QuestionModel> questions = questionService.findAll();
+    return ResponseEntity.ok().body(questions);
+
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<QuestionModel> getEntityById(@PathVariable final int id){
+    QuestionModel question = questionService.findById(id);
+    return ResponseEntity.ok().body(question);
+  }
+
+
 }
