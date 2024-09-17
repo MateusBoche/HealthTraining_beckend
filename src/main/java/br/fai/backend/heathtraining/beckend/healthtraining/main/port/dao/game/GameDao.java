0b33964e9acf84e43ab432051837d1e7754c02dd
Marcos <marcosrodrigues-srs1@hotmail.com>
2024-09-16 21:38:12 -0300

    feat: criacao da interface GameDao

---
+++ src/main/java/br/fai/backend/heathtraining/beckend/healthtraining/main/port/dao/game/GameDao.java
@@ -0,0 +1,7 @@
+package br.fai.backend.heathtraining.beckend.healthtraining.main.port.dao.game;
+
+import br.fai.backend.heathtraining.beckend.healthtraining.main.domain.GameModel;
+import br.fai.backend.heathtraining.beckend.healthtraining.main.port.dao.crud.CrudDao;
+
+public interface GameDao extends CrudDao<GameModel> {
+}
