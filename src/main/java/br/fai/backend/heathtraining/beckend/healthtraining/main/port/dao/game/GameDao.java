fb2ffeec18b79fd3534b6a78652a56a7693de8d4
Marcos <marcosrodrigues-srs1@hotmail.com>
2024-09-16 21:44:03 -0300

    feat: mudanca no GameDao

--- src/main/java/br/fai/backend/heathtraining/beckend/healthtraining/main/port/dao/game/GameDao.java
+++ src/main/java/br/fai/backend/heathtraining/beckend/healthtraining/main/port/dao/game/GameDao.java
@@ -1,7 +1,10 @@
 package br.fai.backend.heathtraining.beckend.healthtraining.main.port.dao.game;
 
 import br.fai.backend.heathtraining.beckend.healthtraining.main.domain.GameModel;
+import br.fai.backend.heathtraining.beckend.healthtraining.main.port.dao.crud.CreateDao;
 import br.fai.backend.heathtraining.beckend.healthtraining.main.port.dao.crud.CrudDao;
+import br.fai.backend.heathtraining.beckend.healthtraining.main.port.dao.crud.DeleteDao;
+import br.fai.backend.heathtraining.beckend.healthtraining.main.port.dao.crud.ReadDao;
 
-public interface GameDao extends CrudDao<GameModel> {
+public interface GameDao extends CreateDao<GameModel>, DeleteDao, ReadDao<GameModel> {
 }
