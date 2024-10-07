e1db0a81e78f84f0eca4b02fdaa0a6882998fbad
Marcos Rodrigues Teixeira <marcosrodrigues-srs1@hotmail.com>
2024-10-07 20:34:18 -0300

    feat: implementacao do AutheticationServiceImpl

--- src/main/java/br/fai/backend/heathtraining/beckend/healthtraining/main/service/authentication/AuthenticationServiceImpl.java
+++ src/main/java/br/fai/backend/heathtraining/beckend/healthtraining/main/service/authentication/AuthenticationServiceImpl.java
@@ -1,10 +1,24 @@
 package br.fai.backend.heathtraining.beckend.healthtraining.main.service.authentication;
 
+import br.fai.backend.heathtraining.beckend.healthtraining.main.domain.UserModel;
+import br.fai.backend.heathtraining.beckend.healthtraining.main.port.dao.user.UserDao;
 import br.fai.backend.heathtraining.beckend.healthtraining.main.port.service.authentication.AuthenticationService;
 
+
 public class AuthenticationServiceImpl implements AuthenticationService {
+
+    private final UserDao userDao;
+
+    public AuthenticationServiceImpl(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
     @Override
     public void authentication(String email, String senha) {
 
+        UserModel user = userDao.readByEmail(email);
+        if (user == null || !user.getSenha().equals(senha)){
+            throw new RuntimeException();
+        }
     }
 }
