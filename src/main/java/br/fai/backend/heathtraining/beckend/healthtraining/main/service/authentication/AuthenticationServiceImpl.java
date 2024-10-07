c0d261865bbcb01f11049c94e9e22e393aea4f56
Marcos Rodrigues Teixeira <marcosrodrigues-srs1@hotmail.com>
2024-10-07 19:57:36 -0300

    feat: inicio da implementacao do AutheticationService

---
+++ src/main/java/br/fai/backend/heathtraining/beckend/healthtraining/main/service/authentication/AuthenticationServiceImpl.java
@@ -0,0 +1,10 @@
+package br.fai.backend.heathtraining.beckend.healthtraining.main.service.authentication;
+
+import br.fai.backend.heathtraining.beckend.healthtraining.main.port.service.authentication.AuthenticationService;
+
+public class AuthenticationServiceImpl implements AuthenticationService {
+    @Override
+    public void authentication(String email, String senha) {
+
+    }
+}
