cf22773fc13b32ac403d0e1e358b01f578d586ae
Marcos <marcosrodrigues-srs1@hotmail.com>
2024-12-01 18:52:41 -0300

    refactor: adicionando ReadGamesByEmail

--- src/main/java/br/fai/backend/heathtraining/beckend/healthtraining/main/port/dao/game/ReadGamesByEmail.java
+++ src/main/java/br/fai/backend/heathtraining/beckend/healthtraining/main/port/dao/game/ReadGamesByEmail.java
@@ -4,6 +4,6 @@ import br.fai.backend.heathtraining.beckend.healthtraining.main.domain.GameModel
 
 import java.util.List;
 
-public interface ReadGamesById {
-  List<GameModel> readGamesById(int id);
+public interface ReadGamesByEmail {
+  List<GameModel> readGamesById(String email);
 }
--- src/main/java/br/fai/backend/heathtraining/beckend/healthtraining/main/port/service/game/ReadGamesByEmail.java
+++ src/main/java/br/fai/backend/heathtraining/beckend/healthtraining/main/port/service/game/ReadGamesByEmail.java
@@ -4,6 +4,6 @@ import br.fai.backend.heathtraining.beckend.healthtraining.main.domain.GameModel
 
 import java.util.List;
 
-public interface ReadGamesById {
-  List<GameModel> readGamesById(final int id);
+public interface ReadGamesByEmail {
+  List<GameModel> readGamesByEmail(final String email);
 }
